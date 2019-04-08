22273f8aea4bea03ae209922260f09bed67736ef
pd18aaq <pd18aaq@ENT-D410-33.herts.ac.uk>
2019-04-08 13:14:33 +0100

    title to empty string

---
+++ test/embeddedmediaplayer/ClipTest.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//test title to empty string
+package embeddedmediaplayer;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author comqsjb
+ */
+public class ClipTest {
+    
+    public ClipTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getTitle method, of class Clip.
+     */
+    
+      
+    @Test
+    public void testSetTitleToEmptyStringKeepsPreviousValue() {
+        System.out.println("NewTitle");
+        Clip instance = new Clip();
+        String OriginalTitle = "Original Title";
+        instance.setTitle(OriginalTitle);              // try to set Empty Title
+        String EmptyTitle = "";
+        instance.setTitle(EmptyTitle);              // try to set Empty Title
+        String ModifiedTitle = instance.getTitle(); // check Empty Title is set or not 
+        assertTrue(OriginalTitle.equals(ModifiedTitle));// compare original title and resulted title
+        System.out.println("Empty Title is not set");
+    }
+    
+ 
+    
+    
+    
+    
+}
